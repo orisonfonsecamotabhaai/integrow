aa68578fb87436d49cf4baab54a2fb75c010bbdf
orisonfonsecamotabhaai <orison.fonseca@motabhaai.com>
2021-10-07 13:30:31 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Login from "./pages/login/Login";
 import { Layout } from "antd";
 import HeaderBar from "./components/header/HeaderBar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Project from "./pages/pages/Project";
+import Project from "./pages/project/Project";
 import { AddProject } from "./pages/addProject/AddProject";
 import { Provider } from "react-redux";
 import store from "./redux/store";
---
+++ src/assets/constants/colours.js
@@ -0,0 +1,8 @@
+const Colours = {
+    lightBlue: '#5F7096',
+    blue: '#2C3D63',
+    darkBlue: '#172B4D',
+    green: '#0dad27'
+  };
+  
+  export default Colours;
